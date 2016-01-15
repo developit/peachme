c78d02cf1fd3919980e28caeec0fb6a03c42ceed
Jason Miller <jason@developit.ca>
2016-01-14 21:04:50 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+peachme
+=======
+
+Auto-follow users on Peach.
+
+Getting Started
+---------------
+
+```sh
+# clone it
+git clone git@github.com:developit/peachme.git
+cd peachme
+
+# Run it
+PORT=8080 npm start
+```
---
+++ server/index.js
@@ -0,0 +1,45 @@
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import bodyParser from 'body-parser';
+import Peach from 'peach-client';
+import request from 'request-promise';
+
+let app = express();
+app.server = http.createServer(app);
+
+app.use(compression());
+
+app.use(cors());
+
+app.use(bodyParser.json({ limit :'100kb' }));
+
+let peach = new Peach({
+	email: process.env.PEACH_EMAIL,
+	password: process.env.PEACH_PASSWORD
+});
+
+peach.request = (...args) => request(peach.getOptions(...args));
+
+function follow({ id, name }, callback) {
+	peach.login().then( () => {
+		peach.getConnections().then( c => {
+			let reqs = c && c.data && c.data.inboundFriendRequests || [],
+				toFriend = reqs.filter( r => (r.stream.id===id || r.stream.name===name) )[0];
+			if (!toFriend) return callback('Not found');
+			peach.request(`/friend-request/${encodeURIComponent(toFriend.id)}/accept`, 'POST').then( () => callback(null, 'Accepted') );
+		});
+	});
+}
+
+app.post('/follow', (req, res) => {
+	follow(req.body, (err, message) => {
+		if (err) res.sendStatus(500);
+		else res.json({ message });
+	});
+});
+
+app.server.listen(process.env.PORT || 8080);
+
+console.log(`Started on port ${app.server.address().port}`);
