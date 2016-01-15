8198a1eda54b23528355facef680d17b46df696e
Jason Miller <jason@developit.ca>
2016-01-15 08:37:44 -0500

    caching

--- server/index.js
+++ server/index.js
@@ -22,17 +22,40 @@ let peach = new Peach({
 
 peach.request = (...args) => request(peach.getOptions(...args));
 
+function login(callback) {
+	let now = Date.now();
+	if ( (now - peach.lastLogin) < 60 * 1000 ) return callback();
+	peach.lastLogin = now;
+	if (peach.currentLogin) return peach.currentLogin.then(callback);
+	peach.currentLogin.then( () => {
+		peach.currentLogin = null;
+		callback();
+	});
+}
+
+function addFriend(friend, callback) {
+	peach.request(`/friend-request/${encodeURIComponent(friend.id)}/accept`, 'POST').then( () => callback(null, 'Accepted') );
+}
+
+peach.cachedReqs = [];
+
 function follow({ id, name }, callback) {
+	let cached = peach.cachedReqs && peach.cachedReqs.filter( r => (r.stream.id===id || r.stream.name===name) )[0];
+	if (cached) return addFriend(cached, callback);
+
 	try {
-		peach.login().then( () => {
+		login( () => {
 			peach.getConnections().then( c => {
 				let reqs = c && c.data && c.data.inboundFriendRequests || [],
 					toFriend = reqs.filter( r => (r.stream.id===id || r.stream.name===name) )[0];
+				peach.cachedReqs = reqs;
 				if (!toFriend) return callback('Not found');
-				peach.request(`/friend-request/${encodeURIComponent(toFriend.id)}/accept`, 'POST').then( () => callback(null, 'Accepted') );
+				addFriend(toFriend, callback);
 			});
 		});
-	} catch(e) {}
+	} catch(e) {
+		callback('Unknown error');
+	}
 }
 
 app.post('/follow', (req, res) => {
